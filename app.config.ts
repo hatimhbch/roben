95ce13fdd33b035e7ac89841109f4c305c9823e1
hatimhbch <158487800+hatimhbch@users.noreply.github.com>
2024-12-23 15:29:25 +0100

    solve 404
    remove wrangler and rel preconnect

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,3 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({
-  server: {
-    preset: "cloudflare_module",
-    compatibilityDate: "2024-12-21",  
-  },
-});
+export default defineConfig({});
--- src/app.tsx
+++ src/app.tsx
@@ -20,9 +20,9 @@ export default function App() {
   return (  
     <Router root={props => (
       <MetaProvider>
-          <Link rel="preconnect" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Sans+SC:wght@100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
-          <Link rel="preconnect" href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-          <Link rel="preconnect" href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+          <Link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Noto+Sans+SC:wght@100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
+          <Link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+          <Link href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
           <Meta name="google-site-verification" content="nNuai59cmnHM4IG2H1QGB-jZ7IPDPFLM8fATxUGdNK8" />
         <ThemeProvider>
           <Nav />
